83a960d069453a61bf572c8afab8fe7ec3c6a1be
cgonzalezsanc <carlosgonzalezsanc@gmail.com>
2016-11-06 12:02:24 +0100

    Modificado flickr.js para que sea seguro

--- flickr.js
+++ flickr.js
@@ -1,5 +1,5 @@
 function add_images(tag) {
-	var url_api = "http://api.flickr.com/services/feeds/photos_public.gne?tags=" + tag +"&tagmode=any&format=json&jsoncallback=?";//Al añadir callback=? se trata como jsonp
+	var url_api = "https://api.flickr.com/services/feeds/photos_public.gne?tags=" + tag +"&tagmode=any&format=json&jsoncallback=?";//Al añadir callback=? se trata como jsonp
 	$.getJSON(url_api).done(function(data) {
 		$.each(data.items, function(i, item) {
 			var img = "<img src='" + item.media.m + "'>";
