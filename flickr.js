9309433020b33f78a02537e41146fa5193808204
cgonzalezsanc <carlosgonzalezsanc@gmail.com>
2016-11-06 11:57:32 +0100

    Archivos subidos

---
+++ flickr.js
@@ -0,0 +1,18 @@
+function add_images(tag) {
+	var url_api = "http://api.flickr.com/services/feeds/photos_public.gne?tags=" + tag +"&tagmode=any&format=json&jsoncallback=?";//Al añadir callback=? se trata como jsonp
+	$.getJSON(url_api).done(function(data) {
+		$.each(data.items, function(i, item) {
+			var img = "<img src='" + item.media.m + "'>";
+			$("#imagenes").after(img);
+		});
+	});
+}
+
+$(document).ready(function() {
+	var tag = "fuenlabrada";
+	add_images(tag);
+	$("#boton").click(function() {
+		tag = $("input").val();
+		add_images(tag);
+	});
+});
